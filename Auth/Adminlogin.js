792a4b3058bfa1bad5f023f262c73378ab574af7
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-03-23 22:51:14 +0530

    Adminlogin

---
+++ Auth/Adminlogin.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const con = require("../Connection/Connection");
+
+const Adminlogin = async (req, res) => {
+  const { adminnumber, adminpassword } = req.body;
+  try {
+    await con.query(
+      `select * from admin where admin_number=${adminnumber}`,
+      async (error, result) => {
+        if (result.length === 0) {
+          res.json({ status: false, msg: "Account not Found" });
+        } else if (adminpassword=== result[0].admin_password) {
+          var p = await jwt.sign(
+            { id: result[0].userid },
+            process.env.jwt_token,
+            { expiresIn: "3d" }
+          );
+          res.json({
+            status: true,
+            admin_jwt: p,
+            msg: "Login Successfull",
+            username: result[0].admin_name,
+          });
+        } else {
+          res.json({ status: false, msg: "Wrong Password" });
+        }
+      }
+    );
+  } catch (err) {
+    res.json({ status: false, msg: "Error", error: err });
+  }
+};
+
+module.exports = Adminlogin;
--- Routes.js
+++ Routes.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const Adminlogin = require("./Auth/Adminlogin");
 const Login = require("./Auth/login");
 const Register = require("./Auth/Register");
 const Getproducts = require("./Products/Getproducts");
@@ -10,7 +11,7 @@ const router=express.Router();
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/getproducts").get(Getproducts);
-
+router.route("/admin/login").post(Adminlogin);
 
 
 
